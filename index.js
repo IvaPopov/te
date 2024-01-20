17327cb7eb9b5e6b6c18e4cbd4d2fa45c6c06974
IvaPopov <151994256+IvaPopov@users.noreply.github.com>
2024-01-20 14:17:28 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ ctx.fillRect(0,0,100,100);
 ctx.fillStyle="#0000ff";
 ctx.fillRect(100,100,200,200);
 ctx.stroke();
+
+ctx.moveTo(300,300);
+ctx.lineTo(400,400);
+ctx.stroke();
+
+ctx.strokeStyle='#ff0000'
+ctx.moveTo(320,300);
+ctx.lineTo(420,400);
+ctx.stroke();
