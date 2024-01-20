f7b52c74c92fb9b6e3302701e838dcfc2b837b87
IvaPopov <151994256+IvaPopov@users.noreply.github.com>
2024-01-20 14:36:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const canvasPlot=document.getElementById("canvas_plot");
-const ctx=canvasPlot.getContext("2D");
-
-ctx.fillRect(0,0,100,100);
-ctx.fillStyle="#0000ff";
-ctx.fillRect(100,100,200,200);
-ctx.stroke();
-
-ctx.strokeStyle="#ff0000"
-ctx.moveTo(300,300);
-ctx.lineTo(400,400);
-ctx.stroke();
-
+const canvasPlot = document.getElementById('canvas_plot');
+const ctx=canvasPlot.getContext('2D');
+const canvasPlot
